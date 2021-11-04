becdc3fcb794b93186ac91f0218edb4e53a217d8
azizullo2003 <gulamovmazizullo@gmail.com>
2021-11-04 21:50:16 +0500

    Initial Commit

---
+++ MyFirst SwiftUIApp/MyFirst_SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirst_SwiftUIAppApp.swift
+//  MyFirst SwiftUIApp
+//
+//  Created by MUHAMMAD AZIZULLO on 04/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFirst_SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
